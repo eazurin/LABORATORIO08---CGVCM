8f60d588abab5f8d553884c397803208470e6917
EBERTH WILFREDO AZURIN ZUÑIGA <88208531+eazurin@users.noreply.github.com>
2025-06-24 17:08:40 -0500

    Create nuevaImagen.py

---
+++ nuevaImagen.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+# Cargar imágenes
+img1 = cv2.imread('img1.jpg')
+img2 = cv2.imread('img2.jpg')
+img3 = cv2.imread('img3.jpg')
+
+# Obtener tamaños
+h1, w1 = img1.shape[:2]
+h2, w2 = img2.shape[:2]
+h3, w3 = img3.shape[:2]
+
+# Obtener el tamaño máximo
+max_height = max(h1, h2, h3)
+max_width = max(w1, w2, w3)
+
+# Redimensionar todas
+img1_resized = cv2.resize(img1, (max_width, max_height))
+img2_resized = cv2.resize(img2, (max_width, max_height))
+img3_resized = cv2.resize(img3, (max_width, max_height))
+
+merged_img = np.zeros_like(img1_resized) # Imagen vacía
+
+merged_img[:,:,0] = img3_resized[:,:,0]  # Canal azul
+merged_img[:,:,1] = img2_resized[:,:,1]  # Canal verde
+merged_img[:,:,2] = img1_resized[:,:,2]  # Canal rojo
+
+cv2.imwrite('merged_img.jpg', merged_img)
+
+cv2.imshow('Imagen combinada', merged_img)
+
+cv2.waitKey(0)
+
